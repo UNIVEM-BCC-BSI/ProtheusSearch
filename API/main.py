4198ef9c81eb5713f43c6d5fefa8168e2aca0a49
Lucas <lumeirellespro@gmail.com>
2023-10-26 22:11:02 -0300

    Update API

--- API/main.py
+++ API/main.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
-    
+import json
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Home"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(debug=True)
+# read file
+with open('tasks.json', 'r') as myfile:
+    data=myfile.read()
+# parse file
+obj = json.loads(data)
+@app.route('/todo/getall',methods=['GET'])
+def getTasks():
+    return jsonify(obj)
