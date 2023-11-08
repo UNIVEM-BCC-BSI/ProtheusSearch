f949f156710079820a4846df1be7b64378507118
MeirellesDev <lumeirellespro@gmail.com>
2023-11-08 20:18:55 -0300

    Ambiente Virtual/ requirements.txt

--- API/Server/instance.py
+++ API/Server/instance.py
@@ -1,76 +1,33 @@
-from flask import Flask, request
-from flask_restx import Api, Resource, fields
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app = Flask(__name)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dados.db'
 db = SQLAlchemy(app)
 
-# Configurar o Flask-RESTx
-api = Api(app, version='1.0', title='API de Exemplo', description='Exemplo de API com Flask-RESTx')
-
-# Definir o modelo da tabela
-resource_fields = api.model('Resource', {
-    'id': fields.Integer,
-    'nome': fields.String,
-    'idade': fields.Integer
-})
-
-# Definir o modelo do banco de dados
-class Tabela(db.Model):
+class Dado(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(80))
-    idade = db.Column(db.Integer)
+    nome = db.Column(db.String(100))
+    valor = db.Column(db.String(100))
 
-db.create_all()
+@app.route('/', methods=['POST'])
+def adicionar_dado():
+    data = request.json
+    nome = data['nome']
+    valor = data['valor']
 
-@api.route('/create')
-class CreateResource(Resource):
-    @api.expect(resource_fields)
-    def post(self):
-        data = request.json
-        nome = data['nome']
-        idade = data['idade']
+    novo_dado = Dado(nome=nome, valor=valor)
+    db.session.add(novo_dado)
+    db.session.commit()
 
-        new_record = Tabela(nome=nome, idade=idade)
-        db.session.add(new_record)
-        db.session.commit()
-        return {'message': 'Registro criado com sucesso'}, 201
+    return jsonify({'message': 'Dado adicionado com sucesso'}), 201
 
-@api.route('/read')
-class ReadResource(Resource):
-    def get(self):
-        records = Tabela.query.all()
-        result = [{'id': record.id, 'nome': record.nome, 'idade': record.idade} for record in records]
-        return result
-
-@api.route('/update/<int:id>')
-class UpdateResource(Resource):
-    @api.expect(resource_fields)
-    def put(self, id):
-        data = request.json
-        nome = data['nome']
-        idade = data['idade']
-
-        record = Tabela.query.get(id)
-        if not record:
-            api.abort(404, 'Registro não encontrado')
-
-        record.nome = nome
-        record.idade = idade
-        db.session.commit()
-        return {'message': f'Registro com ID {id} atualizado com sucesso'}
-
-@api.route('/delete/<int:id>')
-class DeleteResource(Resource):
-    def delete(self, id):
-        record = Tabela.query.get(id)
-        if not record:
-            api.abort(404, 'Registro não encontrado')
-
-        db.session.delete(record)
-        db.session.commit()
-        return {'message': f'Registro com ID {id} excluído com sucesso'}
+@app.route('/buscar_dados', methods=['GET'])
+def buscar_dados():
+    dados = Dado.query.all()
+    resultado = [{'nome': dado.nome, 'valor': dado.valor} for dado in dados]
+    return jsonify(resultado)
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
